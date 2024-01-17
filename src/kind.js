c633fc7550ca8aa9ec983428066709cbba100c54
Ryan Morr <rm.morr@gmail.com>
2024-01-17 14:57:40 -0500

    Initial implementation

--- src/kind.js
+++ src/kind.js
@@ -1,1 +1,20 @@
+const types = new Map();
 
+function getClass(obj) {
+    return {}.toString.call(obj).slice(8, -1).toLowerCase();
+}
+
+export function kind(obj, type) {
+    const check = types.get(type);
+    return check(obj);
+}
+
+export function def(name, check) {
+    const type = Symbol.for(name);
+    types.set(type, check);
+    return type;
+}
+
+export const KIND_STRING = def('string', (obj) => getClass(obj) === 'string');
+export const KIND_NUMBER = def('number', (obj) => getClass(obj) === 'number');
+export const KIND_BOOLEAN = def('boolean', (obj) => getClass(obj) === 'boolean');
--- test/specs/kind.js
+++ test/specs/kind.js
@@ -1,1 +1,23 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { kind, KIND_STRING, KIND_NUMBER, KIND_BOOLEAN, KIND_ARRAY, KIND_FUNCTION} from '../../src/kind.js';
 
+describe('kind', () => {
+    it('should detect a string', () => {
+        expect(kind('foo', KIND_STRING)).to.equal(true);
+        expect(kind(new String('foo'), KIND_STRING)).to.equal(true);
+    });
+
+    it('should detect a number', () => {
+        expect(kind(123, KIND_NUMBER)).to.equal(true);
+        expect(kind(new Number(123), KIND_NUMBER)).to.equal(true);
+        expect(kind(1.2435, KIND_NUMBER)).to.equal(true);
+    });
+
+    it('should detect a boolean', () => {
+        expect(kind(true, KIND_BOOLEAN)).to.equal(true);
+        expect(kind(false, KIND_BOOLEAN)).to.equal(true);
+        expect(kind(new Boolean(true), KIND_BOOLEAN)).to.equal(true);
+        expect(kind(new Boolean(false), KIND_BOOLEAN)).to.equal(true);
+    });
+});
